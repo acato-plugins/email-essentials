ef9d72a1a77b264d730c7c361d884519e2c88842
Remon Pel <remon@acato.nl>
2025-11-18 13:19:53 +0100

    WP <62 compat and email template naming change

--- lib/deprecation.php
+++ lib/deprecation.php
@@ -46,7 +46,18 @@ require_once __DIR__ . '/filter-deprecation.php';
 add_action( 'after_setup_theme', function () {
 	$tpl = locate_template( [ 'wpes-email-template.php' ], false, false );
 	if ( $tpl ) {
-		_deprecated_file( $tpl, '6.0.0', 'Use the `email-essentials-email-template.php` filename instead.' );
+		add_action( 'admin_notices', function () use ( $tpl ) {
+			?>
+			<div class="notice notice-warning is-dismissible">
+				<p>
+					<?php
+					// translators: 1: the old template file name, 2: the new template file name.
+					echo wp_kses_post( sprintf( __( 'The email template file <code>%1$s</code> is deprecated and has been replaced by <code>%2$s</code>. Please copy the new template file into your theme folder and remove the old one.', 'email-essentials' ), 'wpes-email-template.php', 'email-essentials-email-template.php' ) );
+					?>
+				</p>
+			</div>
+			<?php
+		} );
 		// and rename the file to prevent further usage.
 		rename( $tpl, str_replace( 'wpes-', 'email-essentials-', $tpl ) );
 	}
